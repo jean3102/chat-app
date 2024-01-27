4e85bbdb50aaecfa0ca13df7a3fb6d0f5b1fa166
jean carlos <jeancarlos3102@gmail.com>
2024-01-27 14:26:07 -0500

    deploy app

---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+	return <div>Chat application</div>;
+};
+
+export default App;
---
+++ client/src/views/Container.tsx
@@ -0,0 +1,10 @@
+import './container.css';
+const Container = () => {
+	return (
+		<main className="container">
+			<section className="contact"></section>
+		</main>
+	);
+};
+
+export default Container;
