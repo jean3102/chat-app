1fcd5eb70ca1df9950188882e96ff2d35407ed20
jean carlos <jeancarlos3102@gmail.com>
2024-03-29 12:35:24 -0400

    fix bug cors

--- server/src/config/socketIo.ts
+++ server/src/config/socketIo.ts
@@ -1,15 +1,27 @@
 import { createServer } from 'node:http';
 import express from 'express';
 import { Server } from 'socket.io';
-import { CORS } from './ennVariables';
+import { CORS } from './envConf';
+import cors from 'cors';
 
 const app = express();
-const httpServer = createServer(app);
+const corsOptions = {
+	origin: CORS, // Replace with your frontend domain
+	methods: ['GET', 'POST'], // Specify the allowed HTTP methods
+	allowedHeaders: ['Content-Type'], // Specify the allowed headers
+	credentials: true, // Allow sending cookies
+};
 
+app.use(cors(corsOptions));
+
+const httpServer = createServer(app);
 const io = new Server(httpServer, {
 	cors: {
 		origin: CORS,
+		methods: ['GET', 'POST'],
 	},
+	pingTimeout: 60000, // Increase the ping timeout to 60 seconds
+	pingInterval: 25000, // Send a ping every 25 seconds
 });
 
 export { httpServer, io };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,10 @@
 import { connection } from './config/connection';
-import { NODE_PORT } from './config/ennVariables';
+import { NODE_PORT } from './config/envConf';
 import { httpServer } from './config/socketIo';
 
 //*channels
 connection();
+
 httpServer.listen(NODE_PORT, () => {
 	console.log(`server listening on port: ${NODE_PORT}`);
 });
